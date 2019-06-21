d23e2134859b1b990870c8a54b4fc81b187d4f63
pi.karasev <pi.karasev@jet.msk.su>
2019-06-21 17:12:16 +0300

    Доделана лаба по чтению из файлов

--- Lab1.5/configfiles.py
+++ Lab1.5/configfiles.py
@@ -1,11 +1,12 @@
 import glob
+import re
 #print(glob.glob('config_files\*.txt'))
 files=glob.glob('config_files\*.txt')
 ip_adresses=[]
 for file in files:
     with open(file) as f:
         for i in f:
-            if i.find("ip address")>=0 and len(i.replace("ip address","").strip()):
+            if i.find("ip address")>=0 and re.match('^[1-9]', i.replace("ip address","").strip()):
                 ip_adresses.append(i.replace("ip address","").strip())
 print(list(set(ip_adresses)))
 print(len(list(set(ip_adresses))))

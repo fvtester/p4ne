b5ee9a0d4beb1ceca5dca396505d2511194971e6
pi.karasev <pi.karasev@jet.msk.su>
2019-06-21 15:21:40 +0300

    Доделана лабораторная на работу с сетевым оборудованием

--- Lab1.3/SNMP.py
+++ Lab1.3/SNMP.py
@@ -5,16 +5,22 @@ result = getCmd(SnmpEngine(),
 	UdpTransportTarget(('10.31.70.107', 161)),
 	ContextData(),
 	ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0)))
-
-result2 = nextCmd('SNMPv2-MIB', 'sysDescr', '10.31.70.107', 161, 0, lexicographicMode=False) # Не идти в глубину
+result2 = nextCmd(SnmpEngine(),
+	CommunityData('public', mpModel=0),
+	UdpTransportTarget(('10.31.70.107', 161)),
+	ContextData(),
+	ObjectType(ObjectIdentity('1.3.6.1.2.1.2.2.1.2')), lexicographicMode=False) # Не идти в глубину
+#result2 = nextCmd('SNMPv2-MIB', 'sysDescr', '10.31.70.107', 161, 0, lexicographicMode=False) # Не идти в глубину
 
 print(result)
 print(result2)
 for i in result:
-    print(i)
+    for j in i[3]:
+        print(j)
 
-for j in result2:
-    print(j[1])
+for z in result2:
+    for y in z[3]:
+        print(y)
 
 #snmp_object = ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0) # По имени MIB-переменной
 
